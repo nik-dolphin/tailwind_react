43d6fc8afdab305b5319cbd7b6f5777115f3e05a
“divyang-dolphin” <nikunj.dolphinwebsolution@gmail.com>
2022-10-21 16:07:13 +0530

    feat: add radio group drom headless into team panel

--- src/compoenent/team.js
+++ src/compoenent/team.js
@@ -1,86 +1,162 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/anchor-has-content */
-import { Tab } from "@headlessui/react";
+import { RadioGroup, Tab } from "@headlessui/react";
 import React, { useState } from "react";
 import { useModeStore } from "../store/mode/store";
 import { classNames } from "../utility/functions";
-import { Recent, Popular, Trending } from "../utility/utils";
+import { Recent, Popular, Trending, plans } from "../utility/utils";
+
+const CheckIcon = (props) => {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" {...props}>
+      <circle cx={12} cy={12} r={12} fill="#fff" opacity="0.2" />
+      <path
+        d="M7 13l3 3 7-7"
+        stroke="#fff"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
 
 const Team = () => {
   const toggleMode = useModeStore((state) => state.toggleMode);
   let [categories] = useState({ Recent, Popular, Trending });
+  const [selected, setSelected] = useState(plans[0]);
   return (
-    <div
-      className={`${
-        toggleMode ? " bg-black" : "bg-white"
-      } flex flex-center justify-center h-[93.5vh]`}
-    >
-      <div className="w-full max-w-md px-2 py-16 sm:px-0">
-        <Tab.Group>
-          <Tab.List className="flex space-x-1 rounded-xl bg-blue-900 p-1">
-            {Object.keys(categories).map((category) => (
-              <Tab
-                key={category}
-                className={({ selected }) =>
-                  classNames(
-                    "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
-                    "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
-                    selected
-                      ? "bg-white shadow"
-                      : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
-                  )
-                }
-              >
-                <div className="flex flex-col sm:flex-row items-center justify-start sm:pl-2">
-                  <img
-                    className="h-8 w-8 rounded-full bg-white"
-                    src="/profile.png"
-                    alt="profile"
-                  />
-                  <div className="pl-2">{category}</div>
-                </div>
-              </Tab>
-            ))}
-          </Tab.List>
-          <Tab.Panels className="mt-2">
-            {Object.values(categories).map((posts, idx) => (
-              <Tab.Panel
-                key={idx}
-                className={classNames(
-                  "rounded-xl bg-white p-3",
-                  "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
-                )}
-              >
-                <ul>
-                  {posts.map((post) => (
-                    <li
-                      key={post.id}
-                      className="relative rounded-md p-3 hover:bg-gray-100"
-                    >
-                      <h3 className="text-sm font-medium leading-5">
-                        {post.title}
-                      </h3>
-                      <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-                        <li>{post.date}</li>
-                        <li>&middot;</li>
-                        <li>{post.commentCount} comments</li>
-                        <li>&middot;</li>
-                        <li>{post.shareCount} shares</li>
-                      </ul>
-                      <a
-                        href="#"
-                        className={classNames(
-                          "absolute inset-0 rounded-md",
-                          "ring-blue-400 focus:z-10 focus:outline-none focus:ring-2"
+    <div className={`${toggleMode ? " bg-black" : "bg-white"} h-[93.5vh]`}>
+      <div className="flex flex-center justify-center">
+        <div className="w-full max-w-md px-2 py-16 sm:px-0">
+          <Tab.Group>
+            <Tab.List className="flex space-x-1 rounded-xl bg-blue-900 p-1">
+              {Object.keys(categories).map((category) => (
+                <Tab
+                  key={category}
+                  className={({ selected }) =>
+                    classNames(
+                      "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
+                      "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+                      selected
+                        ? "bg-white shadow"
+                        : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
+                    )
+                  }
+                >
+                  <div className="flex flex-col sm:flex-row items-center justify-start sm:pl-2">
+                    <img
+                      className="h-8 w-8 rounded-full bg-white"
+                      src="/profile.png"
+                      alt="profile"
+                    />
+                    <div className="pl-2">{category}</div>
+                  </div>
+                </Tab>
+              ))}
+            </Tab.List>
+            <Tab.Panels className="mt-2">
+              {Object.values(categories).map((posts, idx) => (
+                <Tab.Panel
+                  key={idx}
+                  className={classNames(
+                    "rounded-xl bg-white p-3",
+                    "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
+                  )}
+                >
+                  <ul>
+                    {posts.map((post) => (
+                      <li
+                        key={post.id}
+                        className="relative rounded-md p-3 hover:bg-gray-100"
+                      >
+                        <h3 className="text-sm font-medium leading-5">
+                          {post.title}
+                        </h3>
+                        <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
+                          <li>{post.date}</li>
+                          <li>&middot;</li>
+                          <li>{post.commentCount} comments</li>
+                          <li>&middot;</li>
+                          <li>{post.shareCount} shares</li>
+                        </ul>
+                        <a
+                          href="#"
+                          className={classNames(
+                            "absolute inset-0 rounded-md",
+                            "ring-blue-400 focus:z-10 focus:outline-none focus:ring-2"
+                          )}
+                        />
+                      </li>
+                    ))}
+                  </ul>
+                </Tab.Panel>
+              ))}
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      </div>
+      <div className="w-full px-4 py-16">
+        <div className="mx-auto w-full max-w-md">
+          <RadioGroup value={selected} onChange={setSelected}>
+            <RadioGroup.Label className="sr-only">Server size</RadioGroup.Label>
+            <div className="space-y-2">
+              {plans.map((plan) => (
+                <RadioGroup.Option
+                  key={plan.name}
+                  value={plan}
+                  className={({ active, checked }) =>
+                    `${
+                      active
+                        ? "ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-sky-300"
+                        : ""
+                    }
+                  ${
+                    checked ? "bg-sky-900 bg-opacity-75 text-white" : "bg-white"
+                  }
+                    relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none`
+                  }
+                >
+                  {({ active, checked }) => (
+                    <>
+                      <div className="flex w-full items-center justify-between">
+                        <div className="flex items-center">
+                          <div className="text-sm">
+                            <RadioGroup.Label
+                              as="p"
+                              className={`font-medium  ${
+                                checked ? "text-white" : "text-gray-900"
+                              }`}
+                            >
+                              {plan.name}
+                            </RadioGroup.Label>
+                            <RadioGroup.Description
+                              as="span"
+                              className={`inline ${
+                                checked ? "text-sky-100" : "text-gray-500"
+                              }`}
+                            >
+                              <span>
+                                {plan.ram}/{plan.cpus}
+                              </span>{" "}
+                              <span aria-hidden="true">&middot;</span>{" "}
+                              <span>{plan.disk}</span>
+                            </RadioGroup.Description>
+                          </div>
+                        </div>
+                        {checked && (
+                          <div className="shrink-0 text-white">
+                            <CheckIcon className="h-6 w-6" />
+                          </div>
                         )}
-                      />
-                    </li>
-                  ))}
-                </ul>
-              </Tab.Panel>
-            ))}
-          </Tab.Panels>
-        </Tab.Group>
+                      </div>
+                    </>
+                  )}
+                </RadioGroup.Option>
+              ))}
+            </div>
+          </RadioGroup>
+        </div>
       </div>
     </div>
   );
