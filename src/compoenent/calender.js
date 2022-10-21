bc15af8f6284f5d58ee0863a2d5c8f22e61e50f1
“divyang-dolphin” <nikunj.dolphinwebsolution@gmail.com>
2022-10-21 16:48:14 +0530

    fix: fixed timeline items

--- src/compoenent/calender.js
+++ src/compoenent/calender.js
@@ -5,6 +5,7 @@ import moment from "moment";
 import Timeline from "react-calendar-timeline";
 import "react-calendar-timeline/lib/Timeline.css";
 import { useModeStore } from "../store/mode/store";
+import { items } from "../utility/utils";
 
 const Calender = () => {
   const toggleMode = useModeStore((state) => state.toggleMode);
@@ -14,29 +15,6 @@ const Calender = () => {
     { id: 2, title: "group 2" },
   ];
 
-  const items = [
-    {
-      id: 1,
-      group: 1,
-      title: "item 1",
-      start_time: moment(),
-      end_time: moment().add(1, "hour"),
-    },
-    {
-      id: 2,
-      group: 2,
-      title: "item 2",
-      start_time: moment().add(-0.5, "hour"),
-      end_time: moment().add(5.5, "hour"),
-    },
-    {
-      id: 3,
-      group: 1,
-      title: "item 3",
-      start_time: moment().add(2, "hour"),
-      end_time: moment().add(3, "hour"),
-    },
-  ];
   return (
     <div
       className={`${
--- src/utility/utils.js
+++ src/utility/utils.js
@@ -1,3 +1,29 @@
+import moment from "moment"
+
+export const items = [
+  {
+    id: 1,
+    group: 1,
+    title: "item 1",
+    start_time: moment(),
+    end_time: moment().add(1, "hour"),
+  },
+  {
+    id: 2,
+    group: 2,
+    title: "item 2",
+    start_time: moment().add(-0.5, "hour"),
+    end_time: moment().add(5.5, "hour"),
+  },
+  {
+    id: 3,
+    group: 1,
+    title: "item 3",
+    start_time: moment().add(2, "hour"),
+    end_time: moment().add(3, "hour"),
+  },
+];
+
 export const plans = [
   {
     name: "Startup",
